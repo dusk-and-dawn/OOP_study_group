48180927e3548542cd0ad00176e97fb0c595c34e
Ester <egjorek@gmail.com>
2025-11-27 20:57:54 +0100

    class moods

--- Test.py
+++ Test.py
@@ -12,5 +12,20 @@ myString = stringHandler()
 
 print(myString.color)
 
-print("test again")
-print("test third test")
+class StringMoods:
+    def __init__(self):
+        self.moods = {
+            "happy": "green",
+            "sad": "red",
+            "angry": "yellow",
+            "surprised": "blue"
+        }
+
+    def say(self, words, mood="happy" ):
+        color = self.moods.get(mood, "white")
+        cprint(words, color)
+
+handler = StringMoods()
+handler.say("hello", "happy")
+handler.say("Noooooo!!!!", "angry")
+handler.say("What??", "surprised")
