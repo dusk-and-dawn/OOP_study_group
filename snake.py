e3fbc1bb2e1108e4213cf029a4744f3593fb64f0
dusk-and-dawn <david.rotnicki@gmail.com>
2025-12-15 19:08:53 +0100

    added my background class

--- snake.py
+++ snake.py
@@ -1,5 +1,9 @@
 import pygame
 from random import randint
+from pathlib import Path
+
+pygame.init()
+
 
 class Snake():
     def __init__(self, head, length):
@@ -9,13 +13,40 @@ class Snake():
     
 
 class Gamebackground():
-    def __init__(self, width, height):
+    def __init__(self, width, height, color):
         self.width = width
         self.height = height
-    
+        self.color = color
+        pygame.display.init()
+        self.surface = pygame.display.set_mode([self.width, self.height])
+        self.surface.fill(self.color)
+        pygame.display.set_caption("Experimental Snake")
+
+    def show_splash(self):
+        pygame.display.flip()
+
     def spawn_food(self):
-        location = randint(range(self.width*self.height))
+        self.surface.fill(self.color)
+        location = (randint(0, 450), randint(0, 450))
         print(f"food is at {location}")
+        self.surface.blit(food, location)
 
 
-first_snake = Snake()
+background = Gamebackground(500, 500, (50, 168, 82))
+food = pygame.image.load(Path("static/food1.png")).convert_alpha()
+food = pygame.transform.smoothscale(food, (64, 64))
+
+clock = pygame.time.Clock()
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                background.spawn_food()
+
+    background.show_splash()
+    clock.tick(60)
+
+pygame.quit()
