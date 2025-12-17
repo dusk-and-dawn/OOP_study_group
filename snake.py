f2cbc327119786f2bb7a3be52010a65535bf5771
pleocode <pleoleet@gmail.com>
2025-12-17 20:41:36 +0100

    Gamebackground and Snake class adjustments

--- snake.py
+++ snake.py
@@ -4,45 +4,12 @@ from pathlib import Path
 
 pygame.init()
 
-
-class Snake():
-    def __init__(self , head, length=3, spawn=(5, 5)):
-        self.spawn = spawn
-        self.length = length 
-        self.head = head
-        self.grow_size = 0
-    
-    def spawn_worm(self):
-        self.surface.fill(self.color)
-        location = (randint(0, 0), randint(0, 0))
-        print(f"food is at {location}")
-        self.surface.blit(food, location)
-
-    def up(self):
-        # up = (x=x, y+=1)
-
-    def down(self):
-        # down = (x=x, y-=1)
-    
-    def left(self):
-        #left = (x-=1, y=y)
-
-    def right(self):
-        #right = (x+=1, y=y)
-
-    def grow(self):
-        self.grow_size += 1
-
-    def colision(self):
-        pass
-    
-    
-
 class Gamebackground():
-    def __init__(self, width, height, color):
+    def __init__(self, width, height, color, food_location = (randint(0, 450), randint(0, 450))):
         self.width = width
         self.height = height
         self.color = color
+        self.food_location = food_location
         pygame.display.init()
         self.surface = pygame.display.set_mode([self.width, self.height])
         self.surface.fill(self.color)
@@ -50,29 +17,85 @@ class Gamebackground():
 
     def show_splash(self):
         pygame.display.flip()
+        background.update_food()
 
     def spawn_food(self):
         self.surface.fill(self.color)
-        location = (randint(0, 450), randint(0, 450))
-        print(f"food is at {location}")
-        self.surface.blit(food, location)
+        print(f"food is at {self.food_location}")
+        self.surface.blit(food, self.food_location)
+    
+    def update_food(self):
+        self.surface.fill(self.color)
+        print(f"food is at {self.food_location}")
+        self.surface.blit(food, self.food_location)
 
+class Snake(Gamebackground):
+    def __init__(self , length=3, spawn=(5, 5)):
+        self.spawn = spawn
+        self.length = length 
+        self.head = (background.height/2, background.width/2)
+        self.grow_size = 0
+    
+    def spawn_worm(self):
+        location = (background.height/2, background.width/2)
+        background.surface.blit(worm, location)
+
+    def update_snake():
+        pass
+
+    def up(self):
+        # up = (x=x, y+=1)
+        background.surface.fill(background.color)
+        self.head = (self.head[0], self.head[1]-100)
+        background.surface.blit(worm, self.head)
+        print(f"worm is at {self.head}")
+
+    def down(self):
+        # down = (x=x, y-=1)
+        pass
+    
+    def left(self):
+        #left = (x-=1, y=y)
+        pass
+
+    def right(self):
+        #right = (x+=1, y=y)
+        pass
+
+    def grow(self):
+        self.grow_size += 1
+
+    def colision(self):
+        pass
+    
 
 background = Gamebackground(500, 500, (50, 168, 82))
+snake = Snake()
 food = pygame.image.load(Path("static/food1.png")).convert_alpha()
 food = pygame.transform.smoothscale(food, (64, 64))
+worm = pygame.image.load(Path("static/worm.png")).convert_alpha()
+worm = pygame.transform.smoothscale(worm, (64, 64))
 
 clock = pygame.time.Clock()
 running = True
+snake.spawn_worm()
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
+                snake.left()
+            elif event.key == pygame.K_w:
+                snake.up()
+            elif event.key == pygame.K_d:
+                snake.right()
+            elif event.key == pygame.K_s:
+                snake.down()
+            elif event.key == pygame.K_f:
                 background.spawn_food()
-
     background.show_splash()
+    
     clock.tick(60)
 
 pygame.quit()
